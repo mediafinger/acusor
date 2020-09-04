3298ac3b8c00501731504f80766a822fbfeed314
Andreas Finger <webmaster@mediafinger.com>
2020-09-04 22:25:47 +0200

    Convert user.id from Int64 to UUID

--- src/models/base_model.cr
+++ src/models/base_model.cr
@@ -1,4 +1,12 @@
 abstract class BaseModel < Avram::Model
+  macro default_columns
+    # Defines a custom primary key type
+    primary_key id : UUID
+
+    # adds the `created_at` and `updated_at` timestamps
+    timestamps
+  end
+
   def self.database : Avram::Database.class
     AppDatabase
   end
